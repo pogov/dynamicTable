51f66111ab4e1331ee6060b6c34d790451ad7888
Maciej Pogorzelski <maciejpogorzelski@MacBook-Pro-Maciej.local>
2020-11-30 23:38:08 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { tableData } from "./data.js";
+const emptyArr = [];
+function tableCreate(data, selector) {
+    // const isDataAnArray:boolean = Array.isArray(data);
+    // const isdataArrayNotEmpty:boolean = data.length > 0;
+    // if(!isDataAnArray) throw new  Error('Provided data must be an array')
+    // if(!isdataArrayNotEmpty) throw new Error('No data to populate table');
+    const tableElement = document.querySelector(selector);
+    const table = document.createElement("table");
+    const tableHead = table.createTHead();
+    const tableBody = table.createTBody();
+    const sourceObjectForTableHead = data[0];
+    function createTableHeadRow(key) {
+        const headElement = document.createElement("th");
+        headElement.textContent = key;
+        return headElement;
+    }
+    function createTableDataRow(rowData) {
+        const row = document.createElement("tr");
+        rowData.forEach((rowItem) => {
+            const td = document.createElement("td");
+            td.textContent = typeof rowItem === "string" ? rowItem : rowItem.toString();
+            row.append(td);
+        });
+        return row;
+    }
+    Object.keys(sourceObjectForTableHead).forEach((key) => {
+        tableHead.append(createTableHeadRow(key));
+    });
+    data.forEach((dataObject) => {
+        const dataObjectValuesArray = Object.values(dataObject);
+        tableBody.append(createTableDataRow(dataObjectValuesArray));
+    });
+    tableElement.append(table);
+}
+tableCreate(tableData, '.dynamic-table');
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { tableData } from "./data.js";
+
+const emptyArr: Data[] = []
+
+interface Data {
+  _id: string;
+  name: string;
+  age: number;
+  gender: string;
+  company: string;
+  email: string;
+  phone: string;
+  tags: string[];
+}
+
+type RowItemElement = string|number;
+
+function tableCreate(data: Data[], selector:string):void {
+  
+  // const isDataAnArray:boolean = Array.isArray(data);
+  // const isdataArrayNotEmpty:boolean = data.length > 0;
+
+  // if(!isDataAnArray) throw new  Error('Provided data must be an array')
+  // if(!isdataArrayNotEmpty) throw new Error('No data to populate table');
+
+  const tableElement:Element = document.querySelector(selector)!;
+  const table: HTMLTableElement = document.createElement("table");
+  const tableHead: HTMLTableSectionElement = table.createTHead();
+  const tableBody:HTMLTableSectionElement = table.createTBody();
+  const sourceObjectForTableHead: Data = data[0];
+
+  function createTableHeadRow(key: string):HTMLTableHeaderCellElement {
+    const headElement:HTMLTableHeaderCellElement = document.createElement("th");
+    headElement.textContent = key;
+    return headElement;
+  }
+
+  function createTableDataRow(rowData: RowItemElement[]): HTMLTableRowElement {
+    const row:HTMLTableRowElement = document.createElement("tr");
+    rowData.forEach((rowItem: RowItemElement) => {
+      const td:HTMLTableDataCellElement = document.createElement("td");
+      td.textContent = typeof rowItem === "string" ? rowItem : rowItem.toString();
+      row.append(td);
+    });
+    return row;
+  }
+
+  Object.keys(sourceObjectForTableHead).forEach((key: string) => {
+    tableHead.append(createTableHeadRow(key));
+  });
+
+  data.forEach((dataObject: Data) => {
+    const dataObjectValuesArray = Object.values(dataObject);
+    tableBody.append(createTableDataRow(dataObjectValuesArray));
+  });
+
+  tableElement.append(table);
+}
+
+tableCreate(tableData, '.dynamic-table');
